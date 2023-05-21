53475297b999a516c69595a55df138157e6b04b2
amirhossein allami <captain2480722023@gmail.com>
2023-05-21 18:01:48 +0330

    Update

--- js/script.js
+++ js/script.js
@@ -6,4 +6,9 @@ const inputMaxLength = inputElem.getAttribute('maxlength')
 
 inputElem.addEventListener('keyup', function () {
     spanElem.innerHTML = inputMaxLength - inputElem.value.length
+    if (counter.innerHTML === '0') {
+        counter.style.color = '#f15858'
+    } else {
+        counter.style.color = '#58cff1'
+    }
 })
